12ff97a410886fcfac5eda60ac416765daa3a7de
Julianne Adams <julianne@julianneadams.info>
2023-02-21 11:47:14 -0500

    Add: login and logout

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <a href="/api/auth/login">Log In</a>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/logout/index.tsx
@@ -0,0 +1,9 @@
+const LogoutPage = () => {
+  return (
+    <div>
+      <a href="/api/auth/logout">Log In</a>
+    </div>
+  );
+};
+
+export default LogoutPage;
